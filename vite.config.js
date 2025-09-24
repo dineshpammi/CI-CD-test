e3e9b590f331e2c4ea602d1a1ae6830d9de5f54c
dinesh-pammi <dinesh.pammi@dotcod.in>
2025-09-24 12:03:33 +0530

    Config update

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/CI-CD-test/',  // <-- set to your repo name
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    // setupFiles: './src/test/setup.ts', // optional
-    globals: true,
-    base:'/CI-CD-test/'
-  }
 })
